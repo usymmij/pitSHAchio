f215534a84620c60c934f1ab963bda6a68ed6c7c
Khalid Zabalawi <58151955+HikaruSadashi@users.noreply.github.com>
2024-04-07 02:53:22 -0400

    Added header for hashing utility

---
+++ computeserver/include/hashing_utils.h
@@ -0,0 +1,9 @@
+#ifndef HASHING_UTILS_H
+#define HASHING_UTILS_H
+
+#include <string>
+
+//function to compute SHA256 hash of a string
+std::string computeSHA256Hash(const std::string &input);
+
+#endif // HASHING_UTILS_H
